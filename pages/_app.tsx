41a373f47641e9835e97acf6fc081d20c7b8b05a
XNCOAISN <end.pgm@gmail.com>
2022-08-09 22:33:24 +0900

    ✨ Mantine setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,31 @@
-import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import { AppProps } from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = (props: AppProps) => {
+  const { Component, pageProps } = props;
 
-export default MyApp;
+  return (
+    <>
+      <Head>
+        <title>Shimabu IT University</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "light",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
+};
+
+export default App;
