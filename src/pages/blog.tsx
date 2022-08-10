e6addb87ee74eed4ee61e6dcc36607887e296520
XNCOAISN <end.pgm@gmail.com>
2022-08-10 23:11:47 +0900

    ✨ Blogページ作成

---
+++ src/pages/blog.tsx
@@ -0,0 +1,36 @@
+import { Center, Loader, Stack } from "@mantine/core";
+import type { NextPage } from "next";
+import { BlogCard } from "src/components/BlogCard";
+import { Section } from "src/components/Section";
+import { Layout } from "src/layouts";
+
+const BLOG_LIST = Array(10).fill({
+  title: "This is a header",
+  description:
+    "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ",
+  date: "2022.07.11",
+});
+
+const Blog: NextPage = () => {
+  return (
+    <Layout>
+      <Section title="Blog" mt={40}>
+        <Stack spacing="xl">
+          {BLOG_LIST.map((value, index) => (
+            <BlogCard
+              key={index}
+              title={value.title}
+              description={value.description}
+              date={value.date}
+            />
+          ))}
+          <Center>
+            <Loader />
+          </Center>
+        </Stack>
+      </Section>
+    </Layout>
+  );
+};
+
+export default Blog;
