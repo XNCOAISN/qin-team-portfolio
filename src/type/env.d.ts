89451292de64dc6e5e57be729b3b2c49f97e20cd
XNCOAISN <end.pgm@gmail.com>
2022-09-23 04:10:15 +0900

    ✨ 環境変数の型

---
+++ src/type/env.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable unused-imports/no-unused-vars */
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly NEXT_PUBLIC_MICROCMS_DOMAIN: string;
+    readonly NEXT_PUBLIC_MICROCMS_API_KEY: string;
+    readonly TWITTER_ACCESS_TOKEN: string;
+    readonly NEXT_PUBLIC_TWITTER_USERNAME: string;
+    readonly GITHUB_ACCESS_TOKEN: string;
+    readonly GITHUB_USERNAME: string;
+  }
+}
